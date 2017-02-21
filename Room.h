2c1885ee4b9de918f75bc2db03d01f8c213c3248
Andrew <andrewt1993@yahoo.com>
2017-02-21 15:19:21 -0600

    Started prototyping Room object

---
+++ Room.h
@@ -0,0 +1,17 @@
+#include <list>
+
+class Room
+{
+public:
+	static list<Room> roomList;
+	// coordinates for LL and TR corners
+	int lowerLeft[2];
+	int topRight[2];
+
+
+private:
+	Room(int*, int*);
+	// Look through roomList, checking if current room will fit (physically)
+	// should likely be called by constructor
+	bool checkIfFits();
+}
